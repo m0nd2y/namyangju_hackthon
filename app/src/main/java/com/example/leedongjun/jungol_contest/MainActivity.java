3a6b5887ebc006a656579d1f23cf5a05bf8cd168
m0nday <ldj6192@gmail.com>
2018-08-11 17:27:55 +0900

    main and reg page

---
+++ app/src/main/java/com/example/leedongjun/jungol_contest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.leedongjun.jungol_contest;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
